9ddd79b445c37a9e4039ad642355c423f0fc569f
firsttimeoffender <badamasi.abdulrazaq@yahoo.com>
2022-10-16 18:12:49 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,10 +12,10 @@ int main(void)
 	{
 		for (num2 = 0; num2 < 10; num2++)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
 
-			if (digit1 == 9 && digit2 == 9)
+			if (num1 == 9 && num2 == 9)
 				continue;
 
 			putchar(',');
